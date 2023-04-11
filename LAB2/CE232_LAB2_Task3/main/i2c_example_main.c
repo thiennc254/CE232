c4193314b39018b1d9ca74ee90288c51fb3d1478
thiennc254 <thiennc254.work@gmail.com>
2023-04-11 19:33:06 +0700

    Update LAB2

---
+++ LAB2/CE232_LAB2_Task3/main/i2c_example_main.c
@@ -0,0 +1,131 @@
+#include <string.h>
+#include "driver/i2c.h"
+#include "esp_log.h"
+
+#include "ssd1306.h"
+#include "fontlogo.h"
+
+#define SDA_PIN GPIO_NUM_19
+#define SCL_PIN GPIO_NUM_18
+
+#define tag "SSD1306"
+
+void i2c_master_init() {
+	i2c_config_t conf;
+	conf.mode = I2C_MODE_MASTER;
+	conf.sda_io_num = SDA_PIN;
+	conf.scl_io_num = SCL_PIN;
+	conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+	conf.master.clk_speed = 1000000;
+	
+	i2c_param_config(I2C_NUM_0, &conf);
+	i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, 0, 0, 0);
+}
+
+void ssd1306_init() {
+	esp_err_t espRc;
+
+	i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_CHARGE_PUMP, true);
+	i2c_master_write_byte(cmd, 0x14, true);
+
+	i2c_master_write_byte(cmd, OLED_CMD_SET_SEGMENT_REMAP, true); // reverse left-right mapping
+	i2c_master_write_byte(cmd, OLED_CMD_SET_COM_SCAN_MODE, true); // reverse up-bottom mapping
+
+	i2c_master_write_byte(cmd, OLED_CMD_DISPLAY_ON, true);
+	i2c_master_stop(cmd);
+
+	espRc = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	if (espRc == ESP_OK) {
+		ESP_LOGI(tag, "OLED configured successfully");
+	}
+	else {
+		ESP_LOGE(tag, "OLED configuration failed. code: 0x%.2X", espRc);
+	}
+	i2c_cmd_link_delete(cmd);
+}
+
+void task_ssd1306_display_logo() {
+	i2c_cmd_handle_t cmd;
+
+	uint8_t cur_page = 0;
+
+	cmd = i2c_cmd_link_create();
+	i2c_master_start(cmd);
+	i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+	i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+	i2c_master_write_byte(cmd, 0x00, true); // reset column
+	i2c_master_write_byte(cmd, 0x10, true);
+	i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset page
+
+	i2c_master_stop(cmd);
+	i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	i2c_cmd_link_delete(cmd);
+
+	for (uint8_t i = 0; i < 128; i++) {
+		if (i > 0 && i % 16 == 0) {
+			cmd = i2c_cmd_link_create();
+	 		i2c_master_start(cmd);
+	 		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+	 		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
+	 		i2c_master_write_byte(cmd, 0x00, true); // reset column
+	 		i2c_master_write_byte(cmd, 0x10, true);
+	 		i2c_master_write_byte(cmd, 0xB0 | ++cur_page, true); // increment page
+
+	 		i2c_master_stop(cmd);
+	 		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	 		i2c_cmd_link_delete(cmd);
+	    } else {
+			cmd = i2c_cmd_link_create();
+	 		i2c_master_start(cmd);
+	 		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+
+	 		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+	 		i2c_master_write(cmd, logouit_basic_tr[i], 8, true);
+
+	 		i2c_master_stop(cmd);
+	 		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+	 		i2c_cmd_link_delete(cmd);
+	    }
+    }
+	vTaskDelete(NULL);
+}
+
+// display clear function
+/* void task_ssd1306_display_clear(void *ignore) {
+	i2c_cmd_handle_t cmd;
+
+	uint8_t clear[128];
+	for (uint8_t i = 0; i < 128; i++) {
+		clear[i] = 0;
+	}
+	for (uint8_t i = 0; i < 8; i++) {
+		cmd = i2c_cmd_link_create();
+		i2c_master_start(cmd);
+		i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
+		i2c_master_write_byte(cmd, 0xB0 | i, true);
+
+		i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+		i2c_master_write(cmd, clear, 128, true);
+		i2c_master_stop(cmd);
+		i2c_master_cmd_begin(I2C_NUM_0, cmd, 10/portTICK_PERIOD_MS);
+		i2c_cmd_link_delete(cmd);
+	}
+
+	vTaskDelete(NULL);
+} */ 
+
+void app_main(void) {
+	i2c_master_init();
+	ssd1306_init();
+	task_ssd1306_display_logo();
+}
